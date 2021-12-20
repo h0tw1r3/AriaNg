ea406b4e39f35cfb22223fbbcb50b0f81c67ae2e
free-bots <free-bots.to@protonmail.com>
2021-12-20 10:55:06 +0100

    added configuration instructions to README

--- src/scripts/config/buildConfiguration.js
+++ src/scripts/config/buildConfiguration.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('ariaNg').constant('ariaNgBuildConfiguration', {
-        buildVersion: '${ARIANG_VERSION}',
+        buildVersion: 'BOTS-FLAVOR: ${ARIANG_VERSION}',
         buildCommit: '${ARIANG_BUILD_COMMIT}'
     });
 }());
